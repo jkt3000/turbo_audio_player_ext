ab1e9d75539c265921da1d97eedf8e69d9342a21
John Tajima <john@shopify.com>
2015-02-26 14:17:03 -0500

    convert to js, no jquery

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -12,10 +12,101 @@ chrome.extension.sendMessage({}, function(response) {
 	}, 10);
 });
 
-  // console.log("[Turbo] looking for <audio> tags");
-  // $('audio, video').each(function(e){
-  //   TurboPlayer.loadTurboControls($(this));
-  // });
+
+var TP = TP || {};
+TP.loadTurboControls = function(el) {
+  // add controls
+  var rect = el.getBoundingClientRect();
+  var top   = rect.bottom + 2;
+  var left  = rect.left;
+
+  var div = document.createElement('div');
+  div.className = "turbo-audio";
+
+  // el.wrap()
+  TP.wrap(div, el);
+
+  // el.after(template);
+  var wrapper = el.parentNode;
+  wrapper.innerHTML = wrapper.innerHTML + TP.template;
+
+  // find .turob-audio-controls & set styles
+  var ctrlEl = wrapper.childNodes[0].nextSibling;
+  ctrlEl.style.top = top;
+  ctrlEl.style.left = left;
+
+  // init listener for this div
+  for(var i = 0; i < ctrlEl.childNodes.length; i++) {
+    ctrlEl.childNodes[i].addEventListener('click', TP.buttonListener);
+  }
+
+};
+
+TP.wrap = function(wrapper, el) {
+  var parent = el.parentNode;
+  var sibling = el.nextSibling;
+  wrapper.appendChild(el);
+  if (sibling) {
+    parent.insertBefore(wrapper, sibling);
+  } else {
+    parent.appendChild(wrapper);
+  }
+};
+
+TP.template = "<div class='turbo-audio-controls'>" +
+  "<button class='turbo-audio-btn' data-action='rewind' data-value='-60'><< <small>60</small></button>" +
+  "<button class='turbo-audio-btn' data-action='rewind' data-value='-30'><< <small>30</small></button>" +
+  "<button class='turbo-audio-btn turbo-btn-primary' data-action='play' data-value='1'>&#9658;</button>" +
+  "<button class='turbo-audio-btn turbo-btn-danger' data-action='pause'>&#9616;&#9616;</button>" +
+  "<button class='turbo-audio-btn' data-action='play' data-value='2'><small>2x</small>&#9658;</button>" +
+  "<button class='turbo-audio-btn' data-action='play' data-value='4'><small>4x</small>&#9658;</button>" +
+  "<button class='turbo-audio-btn' data-action='forward' data-value='30'><small>30</small> >></button>" +
+  "<button class='turbo-audio-btn' data-action='forward' data-value='60'><small>60</small> >></button>" +
+"</div>";
+
+TP.buttonListener = function(e) {
+  e.preventDefault();
+  var btn    = e.currentTarget;
+  var action = btn.attributes['data-action'].value;
+  var value  = parseInt(btn.attributes['data-value'] && btn.attributes['data-value'].value, 10);
+  var audio  = e.currentTarget.parentNode.previousSibling;
+
+  switch (action) {
+    case 'rewind':
+      audio.currentTime += value;
+      break;
+    case 'forward':
+      audio.currentTime += value;
+      break;
+    case 'play':
+      audio.playbackRate = value;
+      if (audio.paused) {
+        audio.play();
+      }
+      break;
+    case 'pause':
+      audio.pause();
+      break;
+    default: break;
+  }
+};
+
+
+onDomReady(function(){
+  console.log("[Turbo] Starting - waiting for document.ready");
+  ['audio', 'video'].forEach(function(tag){
+    var els = document.getElementsByTagName(tag);
+    for (var i = 0; i < els.length; i++) {
+      var audioEl = els[i];
+      console.log("Found ", tag, audioEl);
+      TP.loadTurboControls(audioEl);
+    };
+  });
+
+  // add listeners
+
+})
+
 
   // $(".turbo-audio-btn").on('click', function(e){
   //   e.preventDefault();
@@ -45,61 +136,5 @@ chrome.extension.sendMessage({}, function(response) {
   // });
 
 
-var TP = TP || {};
-TP.loadTurboControls = function(el) {
-  // add controls
-  var rect = el.getBoundingClientRect();
-  var top   = rect.bottom + 2;
-  var left  = rect.left;
-
-  var div = document.createElement('div');
-  div.className = "turbo-audio";
-
-  // el.wrap()
-  TP.wrap(div, el);
-
-  // el.after(template);
-  var wrapper = el.parentNode;
-  wrapper.innerHTML = wrapper.innerHTML + template;
-  wrapper.style.top = top;
-  wrapper.style.left = left;
-};
-
-TP.wrap = function(wrapper, el) {
-  var parent = el.parentNode;
-  var sibling = el.nextSibling;
-  wrapper.appendChild(el);
-  if (sibling) {
-    parent.insertBefore(wrapper, sibling);
-  } else {
-    parent.appendChild(wrapper);
-  }
-};
-
-
-var template = "\
-<div class='turbo-audio-controls'>\
-  <button class='turbo-audio-btn' data-action='rewind' data-value='-60'><< <small>60</small></button>\
-  <button class='turbo-audio-btn' data-action='rewind' data-value='-30'><< <small>30</small></button>\
-  <button class='turbo-audio-btn turbo-btn-primary' data-action='play' data-value='1'>&#9658;</button>\
-  <button class='turbo-audio-btn turbo-btn-danger' data-action='pause'>&#9616;&#9616;</button>\
-  <button class='turbo-audio-btn' data-action='play' data-value='2'><small>2x</small>&#9658;</button>\
-  <button class='turbo-audio-btn' data-action='play' data-value='4'><small>4x</small>&#9658;</button>\
-  <button class='turbo-audio-btn' data-action='forward' data-value='30'><small>30</small> >></button>\
-  <button class='turbo-audio-btn' data-action='forward' data-value='60'><small>60</small> >></button>\
-</div>\
-";
-
-
-
-
-console.log("[Turbo] Starting - waiting for document.ready");
-['audio', 'video'].forEach(function(tag){
-  var els = document.getElementsByTagName(tag);
-  for (var i = 0; i < els.length; i++) {
-    console.log("Found ", tag, els[i]);
-    TP.loadTurboControls(els[i]);
-  };
-});
 
 
