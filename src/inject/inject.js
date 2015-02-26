1a0155247d203705344d0a9256b7bd79d168c1a6
John Tajima <john@shopify.com>
2015-02-26 13:34:15 -0500

    convert a bunch to raw js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -12,53 +12,70 @@ chrome.extension.sendMessage({}, function(response) {
 	}, 10);
 });
 
-console.log("[Turbo] Starting - waiting for document.ready");
-$(document).ready(function(){
-  console.log("[Turbo] looking for <audio> tags");
-  $('audio, video').each(function(e){
-    TurboPlayer.loadTurboControls($(this));
-  });
+  // console.log("[Turbo] looking for <audio> tags");
+  // $('audio, video').each(function(e){
+  //   TurboPlayer.loadTurboControls($(this));
+  // });
 
-  $(".turbo-audio-btn").on('click', function(e){
-    e.preventDefault();
-    var audio  = $(this).parent().prev()[0];
-    var action = $(this).data('action');
-    var value  = parseInt($(this).data('value'), 10);
+  // $(".turbo-audio-btn").on('click', function(e){
+  //   e.preventDefault();
+  //   var audio  = $(this).parent().prev()[0];
+  //   var action = $(this).data('action');
+  //   var value  = parseInt($(this).data('value'), 10);
 
-    switch (action) {
-      case 'rewind':
-        audio.currentTime += value;
-        break;
-      case 'forward':
-        audio.currentTime += value;
-        break;
-      case 'play':
-        audio.playbackRate = value;
-        if (audio.paused) {
-          audio.play();
-        }
-        break;
-      case 'pause':
-        audio.pause();
-        break;
-      default: break;
-    }
+  //   switch (action) {
+  //     case 'rewind':
+  //       audio.currentTime += value;
+  //       break;
+  //     case 'forward':
+  //       audio.currentTime += value;
+  //       break;
+  //     case 'play':
+  //       audio.playbackRate = value;
+  //       if (audio.paused) {
+  //         audio.play();
+  //       }
+  //       break;
+  //     case 'pause':
+  //       audio.pause();
+  //       break;
+  //     default: break;
+  //   }
 
-  });
-});
+  // });
 
 
-var TurboPlayer = TurboPlayer || {};
-TurboPlayer.loadTurboControls = function(el) {
+var TP = TP || {};
+TP.loadTurboControls = function(el) {
   // add controls
-  var rect = el[0].getBoundingClientRect();
+  var rect = el.getBoundingClientRect();
   var top   = rect.bottom + 2;
   var left  = rect.left;
 
-  el.wrap("<div class='turbo-audio'></div>");
-  el.after(template);
-  el.parent().find(".turbo-audio-controls").css({top:top, left: left});
+  var div = document.createElement('div');
+  div.className = "turbo-audio";
+
+  // el.wrap()
+  TP.wrap(div, el);
+
+  // el.after(template);
+  var wrapper = el.parentNode;
+  wrapper.innerHTML = wrapper.innerHTML + template;
+  wrapper.style.top = top;
+  wrapper.style.left = left;
 };
+
+TP.wrap = function(wrapper, el) {
+  var parent = el.parentNode;
+  var sibling = el.nextSibling;
+  wrapper.appendChild(el);
+  if (sibling) {
+    parent.insertBefore(wrapper, sibling);
+  } else {
+    parent.appendChild(wrapper);
+  }
+};
+
 
 var template = "\
 <div class='turbo-audio-controls'>\
@@ -72,4 +89,17 @@ var template = "\
   <button class='turbo-audio-btn' data-action='forward' data-value='60'><small>60</small> >></button>\
 </div>\
 ";
+
+
+
+
+console.log("[Turbo] Starting - waiting for document.ready");
+['audio', 'video'].forEach(function(tag){
+  var els = document.getElementsByTagName(tag);
+  for (var i = 0; i < els.length; i++) {
+    console.log("Found ", tag, els[i]);
+    TP.loadTurboControls(els[i]);
+  };
+});
+
 
